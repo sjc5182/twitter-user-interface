35bed03dae83580ed127bafa03fd4f996f93af65
sjc5182 <shiguangchen1987@gmail.com>
2019-08-09 13:48:09 -0700

    remove all the files

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: '3kyBFuTNOwV9NU8201i2887gf',
-  consumer_secret: 'JFM7QfgdckrBps36hVKxZYwFLdueUbc0rySLWIvuKDTfPx88AS',
-  access_token_key: '4797283939-U0xGo8UjmobKl992uUeT4wtREOQUGDMsRCBqk5I',
-  access_token_secret: 'qFnabnHQKxxoApDgtnbEN6ECXP22ylNkPy3Dg63ZCSicP'
-}
